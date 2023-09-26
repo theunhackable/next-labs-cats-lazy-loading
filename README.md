cbe1de278226a298960bfdfdf9d5942d79c48c2e
theunhackable <srirangasaipagala@gmail.com>
2023-09-26 16:08:21 +0530

    added api link

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Live Link
 [github live link](https://theunhackable.github.io/next-labs-lazy-loading/)
 [gitlab live link](https://next-labs-lazy-loading-srirangasaipagala-0edfa149417bd2dc46795c.gitlab.io/)
-## Public API
 
-- [cazts.com](https://cataas.com/)
+## Public API
+ - [picsum.photos](https://picsum.photos/)
+
+
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let isLoading = false;
 
 
-let id = 0;
-const LIMIT_ID = 1000;
 
-const loadingElement = document.getElementById('loading');
+
+const dataContainer = document.getElementById('data-container');
 
 function displayImage(imageUrl) {
-  const dataContainer = document.getElementById('data-container');
   const imageElement = document.createElement('img');
+  
   imageElement.classList.add('image');
   imageElement.src = imageUrl;
   imageElement.loading = "lazy";
   imageElement.alt = "random image";
+
   dataContainer.appendChild(imageElement);
 }
 
@@ -22,26 +22,21 @@ async function fetchData() {
   } else {
     isLoading = true;
   }
-  if (id >= LIMIT_ID) {
-    return;
+  
+
+  for (let i = 0; i < 15; i++) {
+
+    fetch('https://picsum.photos/200/300?random=1').then((response) => {
+      displayImage(response.url);
+    }
+    );
+    // displayImage('https://picsum.photos/200/300?random=2')
+      
   }
-
-
-
-  for (let i = id; i < id + 5; i++) {
-    fetch(`https://picsum.photos/id/${i}/info`)
-      .then(response => response.json())
-      .catch(error => console.log(error.message))
-      .then(data => displayImage(data.download_url))
-      .catch(error => console.log(error.message));
-  }
-  id += 5;
-
   isLoading = false;
 }
 // Function to check if the user has scrolled to the bottom of the page
 function handleScroll() {
-  const dataContainer = document.getElementById('data-container');
   if (dataContainer && window.innerHeight + window.scrollY >= dataContainer.offsetHeight) {
     fetchData();
   }
